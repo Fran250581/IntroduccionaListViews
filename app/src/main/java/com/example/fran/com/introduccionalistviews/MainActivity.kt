f0f998c90028f921929bb1f0cf69adaa7c19aa72
Fran250581 <[fran250581@hotmail.es]>
2017-10-18 13:57:54 +0200

    Commit 3

--- app/src/main/java/com/example/fran/com/introduccionalistviews/MainActivity.kt
+++ app/src/main/java/com/example/fran/com/introduccionalistviews/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.fran.com.introduccionalistviews
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.ArrayAdapter
@@ -15,17 +14,41 @@ class MainActivity : ListActivity() {
     public override fun onCreate(bundle: Bundle?) {
         super.onCreate(bundle)
         setContentView(R.layout.activity_main)
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        //val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, items)
 
         listAdapter = arrayAdapter
         selection = findViewById<View>(R.id.elemento_seleccionado) as TextView?
     }
 
+    /*
     override fun onListItemClick(parent: ListView, v: View, position: Int, id: Long) {
         selection!!.text = items[position]
     }
+    */
+
+    public override fun onListItemClick(parent: ListView, v: View, position: Int, id: Long) {
+        val marcados = parent.checkedItemPositions
+        val elementos = StringBuilder()
+
+        if (marcados != null) {
+            for (i in 0 until marcados.size()) {
+                // valueAt(i) valdrá true si el ítem está marcado
+                if (marcados.valueAt(i)) {
+                    // keyAt(i) nos devuelve la posición del elemento
+                    elementos.append(items[marcados.keyAt(i)])
+
+                    elementos.append(" + ")
+                }
+            }
+            elementos.delete(elementos.lastIndexOf(" + "), elementos.length - 1)
+        }
+        selection?.setText(elementos.toString())
+    }
 
     companion object {
-        private val items = arrayOf("En", "un", "lugar", "de", "la", "Mancha", "de", "cuyo", "nombre", "no", "quiero", "acordarme", "no", "ha", "mucho", "tiempo", "que", "vivía", "un", "hidalgo", "de", "los", "de", "lanza", "en", "astillero", "adarga", "antigua", "rocín", "flaco", "y", "galgo", "corredor")
+        private val items = arrayOf("En", "un", "lugar", "de", "la", "Mancha", "de", "cuyo", "nombre",
+                "no", "quiero", "acordarme", "no", "ha", "mucho", "tiempo", "que", "vivía", "un", "hidalgo", "de",
+                "los", "de", "lanza", "en", "astillero", "adarga", "antigua", "rocín", "flaco", "y", "galgo", "corredor")
     }
 }
